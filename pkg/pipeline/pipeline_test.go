757c7543c0e77f27d0c09f4ad065173905371c7c
KalmanMeth <meth@il.ibm.com>
2022-02-21 18:26:51 +0200

    added a simple end-to-end pipeline test (#92)
    * added a simple end-to-end pipeline test
    
    * fixed data on copyright
    
    * removed extra print statements
    
    * combined pipeline tests into single file
    
    * used data from file in repository

--- pkg/pipeline/pipeline_test.go
+++ pkg/pipeline/pipeline_test.go
@@ -18,9 +18,13 @@
 package pipeline
 
 import (
+	"github.com/json-iterator/go"
 	"github.com/netobserv/flowlogs2metrics/pkg/config"
+	"github.com/netobserv/flowlogs2metrics/pkg/pipeline/decode"
+	"github.com/netobserv/flowlogs2metrics/pkg/pipeline/ingest"
 	"github.com/netobserv/flowlogs2metrics/pkg/pipeline/transform"
 	"github.com/netobserv/flowlogs2metrics/pkg/pipeline/write"
+	"github.com/netobserv/flowlogs2metrics/pkg/test"
 	"github.com/stretchr/testify/require"
 	"testing"
 )
@@ -36,4 +40,68 @@ func Test_transformToLoki(t *testing.T) {
 	loki, err := write.NewWriteLoki()
 	loki.Write(transformed)
 	require.NoError(t, err)
+}
+
+const configTemplate = `---
+log-level: debug
+pipeline:
+  ingest:
+    type: file
+    file:
+      filename: ../../hack/examples/ocp-ipfix-flowlogs.json
+  decode:
+    type: json
+  transform:
+    - type: generic
+      generic:
+        - input: Bytes
+          output: fl2m_bytes
+        - input: DstAddr
+          output: fl2m_dstAddr
+        - input: DstPort
+          output: fl2m_dstPort
+        - input: Packets
+          output: fl2m_packets
+        - input: SrcAddr
+          output: fl2m_srcAddr
+        - input: SrcPort
+          output: fl2m_srcPort
+  extract:
+    type: none
+  encode:
+    type: none
+  write:
+    type: none
+`
+
+func Test_SimplePipeline(t *testing.T) {
+	var json = jsoniter.ConfigCompatibleWithStandardLibrary
+	var mainPipeline *Pipeline
+	var err error
+	var b []byte
+	v := test.InitConfig(t, configTemplate)
+	config.Opt.PipeLine.Ingest.Type = "file"
+	config.Opt.PipeLine.Decode.Type = "json"
+	config.Opt.PipeLine.Extract.Type = "none"
+	config.Opt.PipeLine.Encode.Type = "none"
+	config.Opt.PipeLine.Write.Type = "none"
+	config.Opt.PipeLine.Ingest.File.Filename = "../../hack/examples/ocp-ipfix-flowlogs.json"
+
+	val := v.Get("pipeline.transform\n")
+	b, err = json.Marshal(&val)
+	require.NoError(t, err)
+	config.Opt.PipeLine.Transform = string(b)
+
+	mainPipeline, err = NewPipeline()
+	require.NoError(t, err)
+
+	// The file ingester reads the entire file, pushes it down the pipeline, and then exits
+	// So we don't need to run it in a separate go-routine
+	mainPipeline.Run()
+	// What is there left to check? Check length of saved data of each stage in private structure.
+	ingester := mainPipeline.Ingester.(*ingest.IngestFile)
+	decoder := mainPipeline.Decoder.(*decode.DecodeJson)
+	writer := mainPipeline.Writer.(*write.WriteNone)
+	require.Equal(t, len(ingester.PrevRecords), len(decoder.PrevRecords))
+	require.Equal(t, len(ingester.PrevRecords), len(writer.PrevRecords))
 }
