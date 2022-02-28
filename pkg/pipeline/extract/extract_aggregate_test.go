e63d36bb535f7b3016bba61d93292dcae18294e2
Ronen Schaffer <ronen.schaffer@ibm.com>
2022-02-28 10:16:32 +0200

    Refactor test to use test cases

--- pkg/pipeline/extract/extract_aggregate_test.go
+++ pkg/pipeline/extract/extract_aggregate_test.go
@@ -26,7 +26,11 @@ import (
 	"testing"
 )
 
+// This tests extract_aggregate as a whole. It can be thought of as an integration test between extract_aggregate.go and
+// aggregate.go and aggregates.go. The test sends flows in 2 batches and verifies the extractor's output after each
+// batch. The output of the 2nd batch depends on the 1st batch.
 func Test_Extract(t *testing.T) {
+	// Setup
 	yamlConfig := `
 aggregates:
 - name: bandwidth
@@ -52,121 +56,137 @@ aggregates:
 	extractAggregate, err := NewExtractAggregate()
 	require.NoError(t, err)
 
-	input1 := []config.GenericMap{
-		{"service": "http", "bytes": 10.0},
-		{"service": "http", "bytes": 20.0},
-		{"service": "tcp", "bytes": 1.0},
-		{"service": "tcp", "bytes": 2.0},
+	// Test cases
+	tests := []struct {
+		name         string
+		inputBatch   []config.GenericMap
+		expectedAggs []config.GenericMap
+	}{
+		{
+			name: "batch1",
+			inputBatch: []config.GenericMap{
+				{"service": "http", "bytes": 10.0},
+				{"service": "http", "bytes": 20.0},
+				{"service": "tcp", "bytes": 1.0},
+				{"service": "tcp", "bytes": 2.0},
+			},
+			expectedAggs: []config.GenericMap{
+				{
+					"name":            "bandwidth",
+					"record_key":      "bytes",
+					"by":              "service",
+					"aggregate":       "http",
+					"service":         "http",
+					"operation":       aggregate.Operation(aggregate.OperationSum),
+					"value":           "30.000000",
+					"bandwidth_value": "30.000000",
+					"recentRawValues": []float64{10.0, 20.0},
+					"count":           "2",
+				},
+				{
+					"name":            "bandwidth",
+					"record_key":      "bytes",
+					"by":              "service",
+					"aggregate":       "tcp",
+					"service":         "tcp",
+					"operation":       aggregate.Operation(aggregate.OperationSum),
+					"value":           "3.000000",
+					"bandwidth_value": "3.000000",
+					"recentRawValues": []float64{1.0, 2.0},
+					"count":           "2",
+				},
+				{
+					"name":                  "bandwidth_count",
+					"record_key":            "",
+					"by":                    "service",
+					"aggregate":             "http",
+					"service":               "http",
+					"operation":             aggregate.Operation(aggregate.OperationCount),
+					"value":                 "2.000000",
+					"bandwidth_count_value": "2.000000",
+					"recentRawValues":       []float64{1.0, 1.0},
+					"count":                 "2",
+				},
+				{
+					"name":                  "bandwidth_count",
+					"record_key":            "",
+					"by":                    "service",
+					"aggregate":             "tcp",
+					"service":               "tcp",
+					"operation":             aggregate.Operation(aggregate.OperationCount),
+					"value":                 "2.000000",
+					"bandwidth_count_value": "2.000000",
+					"recentRawValues":       []float64{1.0, 1.0},
+					"count":                 "2",
+				},
+			},
+		},
+		{
+			name: "batch2",
+			inputBatch: []config.GenericMap{
+				{"service": "http", "bytes": 30.0},
+				{"service": "tcp", "bytes": 4.0},
+				{"service": "tcp", "bytes": 5.0},
+			},
+			expectedAggs: []config.GenericMap{
+				{
+					"name":            "bandwidth",
+					"record_key":      "bytes",
+					"by":              "service",
+					"aggregate":       "http",
+					"service":         "http",
+					"operation":       aggregate.Operation(aggregate.OperationSum),
+					"value":           "60.000000",
+					"bandwidth_value": "60.000000",
+					"recentRawValues": []float64{30.0},
+					"count":           "3",
+				},
+				{
+					"name":            "bandwidth",
+					"record_key":      "bytes",
+					"by":              "service",
+					"aggregate":       "tcp",
+					"service":         "tcp",
+					"operation":       aggregate.Operation(aggregate.OperationSum),
+					"value":           "12.000000",
+					"bandwidth_value": "12.000000",
+					"recentRawValues": []float64{4.0, 5.0},
+					"count":           "4",
+				},
+				{
+					"name":                  "bandwidth_count",
+					"record_key":            "",
+					"by":                    "service",
+					"aggregate":             "http",
+					"service":               "http",
+					"operation":             aggregate.Operation(aggregate.OperationCount),
+					"value":                 "3.000000",
+					"bandwidth_count_value": "3.000000",
+					"recentRawValues":       []float64{1.0},
+					"count":                 "3",
+				},
+				{
+					"name":                  "bandwidth_count",
+					"record_key":            "",
+					"by":                    "service",
+					"aggregate":             "tcp",
+					"service":               "tcp",
+					"operation":             aggregate.Operation(aggregate.OperationCount),
+					"value":                 "4.000000",
+					"bandwidth_count_value": "4.000000",
+					"recentRawValues":       []float64{1.0, 1.0},
+					"count":                 "4",
+				},
+			},
+		},
 	}
-	expectedAggs1 := []config.GenericMap{
-		{
-			"name":            "bandwidth",
-			"record_key":      "bytes",
-			"by":              "service",
-			"aggregate":       "http",
-			"service":         "http",
-			"operation":       aggregate.Operation(aggregate.OperationSum),
-			"value":           "30.000000",
-			"bandwidth_value": "30.000000",
-			"recentRawValues": []float64{10.0, 20.0},
-			"count":           "2",
-		},
-		{
-			"name":            "bandwidth",
-			"record_key":      "bytes",
-			"by":              "service",
-			"aggregate":       "tcp",
-			"service":         "tcp",
-			"operation":       aggregate.Operation(aggregate.OperationSum),
-			"value":           "3.000000",
-			"bandwidth_value": "3.000000",
-			"recentRawValues": []float64{1.0, 2.0},
-			"count":           "2",
-		},
-		{
-			"name":                  "bandwidth_count",
-			"record_key":            "",
-			"by":                    "service",
-			"aggregate":             "http",
-			"service":               "http",
-			"operation":             aggregate.Operation(aggregate.OperationCount),
-			"value":                 "2.000000",
-			"bandwidth_count_value": "2.000000",
-			"recentRawValues":       []float64{1.0, 1.0},
-			"count":                 "2",
-		},
-		{
-			"name":                  "bandwidth_count",
-			"record_key":            "",
-			"by":                    "service",
-			"aggregate":             "tcp",
-			"service":               "tcp",
-			"operation":             aggregate.Operation(aggregate.OperationCount),
-			"value":                 "2.000000",
-			"bandwidth_count_value": "2.000000",
-			"recentRawValues":       []float64{1.0, 1.0},
-			"count":                 "2",
-		},
-	}
-	actualAggs1 := extractAggregate.Extract(input1)
-	require.ElementsMatch(t, expectedAggs1, actualAggs1)
 
-	input2 := []config.GenericMap{
-		{"service": "http", "bytes": 30.0},
-		{"service": "tcp", "bytes": 4.0},
-		{"service": "tcp", "bytes": 5.0},
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			actualAggs := extractAggregate.Extract(tt.inputBatch)
+			// Since the order of the elements in the returned slice from Extract() is non-deterministic, we use
+			// ElementsMatch() rather than Equals()
+			require.ElementsMatch(t, tt.expectedAggs, actualAggs)
+		})
 	}
-	expectedAggs2 := []config.GenericMap{
-		{
-			"name":            "bandwidth",
-			"record_key":      "bytes",
-			"by":              "service",
-			"aggregate":       "http",
-			"service":         "http",
-			"operation":       aggregate.Operation(aggregate.OperationSum),
-			"value":           "60.000000",
-			"bandwidth_value": "60.000000",
-			"recentRawValues": []float64{30.0},
-			"count":           "3",
-		},
-		{
-			"name":            "bandwidth",
-			"record_key":      "bytes",
-			"by":              "service",
-			"aggregate":       "tcp",
-			"service":         "tcp",
-			"operation":       aggregate.Operation(aggregate.OperationSum),
-			"value":           "12.000000",
-			"bandwidth_value": "12.000000",
-			"recentRawValues": []float64{4.0, 5.0},
-			"count":           "4",
-		},
-		{
-			"name":                  "bandwidth_count",
-			"record_key":            "",
-			"by":                    "service",
-			"aggregate":             "http",
-			"service":               "http",
-			"operation":             aggregate.Operation(aggregate.OperationCount),
-			"value":                 "3.000000",
-			"bandwidth_count_value": "3.000000",
-			"recentRawValues":       []float64{1.0},
-			"count":                 "3",
-		},
-		{
-			"name":                  "bandwidth_count",
-			"record_key":            "",
-			"by":                    "service",
-			"aggregate":             "tcp",
-			"service":               "tcp",
-			"operation":             aggregate.Operation(aggregate.OperationCount),
-			"value":                 "4.000000",
-			"bandwidth_count_value": "4.000000",
-			"recentRawValues":       []float64{1.0, 1.0},
-			"count":                 "4",
-		},
-	}
-	actualAggs2 := extractAggregate.Extract(input2)
-	require.ElementsMatch(t, expectedAggs2, actualAggs2)
-
 }
