65e3443f8cfc42fe95cf6e3551cab44aa77eaf69
Kalman Meth <meth@il.ibm.com>
2022-04-04 12:59:26 +0300

    kafka e2e test code cleanup

--- pkg/test/e2e/kafka/kafka_kind_start.sh
+++ pkg/test/e2e/kafka/kafka_kind_start.sh
@@ -4,14 +4,12 @@ export KIND=../../../../bin/kind-v0.11.1
 
 echo "KIND = " $KIND
 
-$KIND create cluster
-kubectl cluster-info --context kind-kind
+$KIND create cluster --name kafka-e2e-test
+kubectl cluster-info --context kafka-e2e-test
 
 echo
 echo "Installing Kafka"
 echo
 kubectl apply -f ./strimzi.yaml -n default
-sleep 60
 kubectl apply -f ./kafka.strimzi.yaml
-
 
