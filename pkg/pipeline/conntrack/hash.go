cb092ec0a75fb91941751301c1b9bc086307e46d
Ronen Schaffer <ronen.schaffer@ibm.com>
2022-06-09 18:46:00 +0300

    Refactor hashType: []byte -> uint64

--- pkg/pipeline/conntrack/hash.go
+++ pkg/pipeline/conntrack/hash.go
@@ -19,6 +19,7 @@ package conntrack
 
 import (
 	"bytes"
+	"encoding/binary"
 	"encoding/gob"
 	"fmt"
 	"hash"
@@ -28,78 +29,50 @@ import (
 	log "github.com/sirupsen/logrus"
 )
 
-type hashType []byte
-
 // TODO: what's a better name for this struct?
 type totalHashType struct {
-	hashA     hashType
-	hashB     hashType
-	hashTotal hashType
-}
-
-func areHashEqual(h1, h2 hashType) bool {
-	if len(h1) != len(h2) {
-		return false
-	}
-	for i := range h1 {
-		if h1[i] != h2[i] {
-			return false
-		}
-	}
-	return true
-}
-
-func copyTotalHash(h totalHashType) totalHashType {
-	newHashA := make([]byte, len(h.hashA))
-	newHashB := make([]byte, len(h.hashB))
-	newHashTotal := make([]byte, len(h.hashTotal))
-	copy(newHashA, h.hashA)
-	copy(newHashB, h.hashB)
-	copy(newHashTotal, h.hashTotal)
-	return totalHashType{
-		hashA:     newHashA,
-		hashB:     newHashB,
-		hashTotal: newHashTotal,
-	}
+	hashA     uint64
+	hashB     uint64
+	hashTotal uint64
 }
 
 // ComputeHash computes the hash of a flow log according to keyDefinition.
 // Two flow logs will have the same hash if they belong to the same connection.
-func ComputeHash(flowLog config.GenericMap, keyDefinition api.KeyDefinition, hasher hash.Hash) (*totalHashType, error) {
-	fieldGroup2hash := make(map[string]hashType)
+func ComputeHash(flowLog config.GenericMap, keyDefinition api.KeyDefinition, hasher hash.Hash64) (totalHashType, error) {
+	fieldGroup2hash := make(map[string]uint64)
 
 	// Compute the hash of each field group
 	for _, fg := range keyDefinition.FieldGroups {
 		h, err := computeHashFields(flowLog, fg.Fields, hasher)
 		if err != nil {
-			return nil, fmt.Errorf("compute hash: %w", err)
+			return totalHashType{}, fmt.Errorf("compute hash: %w", err)
 		}
 		fieldGroup2hash[fg.Name] = h
 	}
 
 	// Compute the total hash
-	th := &totalHashType{}
+	th := totalHashType{}
 	hasher.Reset()
 	for _, fgName := range keyDefinition.Hash.FieldGroupRefs {
-		hasher.Write(fieldGroup2hash[fgName])
+		hasher.Write(uint64ToBytes(fieldGroup2hash[fgName]))
 	}
 	if keyDefinition.Hash.FieldGroupARef != "" {
 		th.hashA = fieldGroup2hash[keyDefinition.Hash.FieldGroupARef]
 		th.hashB = fieldGroup2hash[keyDefinition.Hash.FieldGroupBRef]
 		// Determine order between A's and B's hash to get the same hash for both flow logs from A to B and from B to A.
-		if bytes.Compare(th.hashA, th.hashB) < 0 {
-			hasher.Write(th.hashA)
-			hasher.Write(th.hashB)
+		if th.hashA < th.hashB {
+			hasher.Write(uint64ToBytes(th.hashA))
+			hasher.Write(uint64ToBytes(th.hashB))
 		} else {
-			hasher.Write(th.hashB)
-			hasher.Write(th.hashA)
+			hasher.Write(uint64ToBytes(th.hashB))
+			hasher.Write(uint64ToBytes(th.hashA))
 		}
 	}
-	th.hashTotal = hasher.Sum([]byte{})
+	th.hashTotal = hasher.Sum64()
 	return th, nil
 }
 
-func computeHashFields(flowLog config.GenericMap, fieldNames []string, hasher hash.Hash) (hashType, error) {
+func computeHashFields(flowLog config.GenericMap, fieldNames []string, hasher hash.Hash64) (uint64, error) {
 	hasher.Reset()
 	for _, fn := range fieldNames {
 		f, ok := flowLog[fn]
@@ -109,11 +82,17 @@ func computeHashFields(flowLog config.GenericMap, fieldNames []string, hasher ha
 		}
 		bytes, err := toBytes(f)
 		if err != nil {
-			return nil, err
+			return 0, err
 		}
 		hasher.Write(bytes)
 	}
-	return hasher.Sum([]byte{}), nil
+	return hasher.Sum64(), nil
+}
+
+func uint64ToBytes(data uint64) []byte {
+	b := make([]byte, 8)
+	binary.BigEndian.PutUint64(b, data)
+	return b
 }
 
 func toBytes(data interface{}) ([]byte, error) {
