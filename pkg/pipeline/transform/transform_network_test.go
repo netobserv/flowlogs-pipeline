68bbddd24448ca3bbabd5c0335f33c6285b0714b
Mario Macias <mmaciasl@redhat.com>
2022-09-09 15:11:27 +0200

    fixed test file paths

--- pkg/pipeline/transform/transform_network_test.go
+++ pkg/pipeline/transform/transform_network_test.go
@@ -124,10 +124,10 @@ func getExpectedOutput() config.GenericMap {
 }
 
 func getServicesDB(t *testing.T) *netdb.ServiceNames {
-	etcProtos, err := os.Open(path.Join("fixtures", "etcProtocols.txt"))
+	etcProtos, err := os.Open(path.Join("netdb", "fixtures", "etcProtocols.txt"))
 	require.NoError(t, err)
 	defer etcProtos.Close()
-	etcSvcs, err := os.Open(path.Join("fixtures", "etcServices.txt"))
+	etcSvcs, err := os.Open(path.Join("netdb", "fixtures", "etcServices.txt"))
 	require.NoError(t, err)
 	defer etcSvcs.Close()
 
