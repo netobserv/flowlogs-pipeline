f466f1f7c56a17fc6c4d23f19b16c48510a90228
Joel Takvorian <joel.takvorian@qaraywa.net>
2022-10-03 11:33:43 +0200

    Unify ingest metrics too (#316)
    * Unify ingest metrics too
    
    Except for netflow/ipfix which continue to use goflow2 metrics, other
    ingester now adopt the same "framework" for metrics as the other stages
    
    * Rename metric ingest_packets_size_bytes => ingest_batch_size_bytes
    
    This is the ingested batch size distribution in bytes, *not* the ingest
    stage output batches.
    Also for kafka, since we don't have access to batch sizes (afaik),
    the other batch size metric will unfortunately always observe 1 (ie.
    increment per flow, like just a counter)

--- pkg/pipeline/ingest/metrics.go
+++ pkg/pipeline/ingest/metrics.go
@@ -6,21 +6,69 @@ import (
 	"github.com/prometheus/client_golang/prometheus"
 )
 
+var (
+	latencyHistogram = operational.DefineMetric(
+		"ingest_latency_ms",
+		"Latency between flow end time and ingest time, in milliseconds",
+		operational.TypeHistogram,
+		"stage",
+	)
+	batchSizeSummary = operational.DefineMetric(
+		"ingest_flows_processed", // This is intentionally named to emphasize its utility for flows counting, despite being a batch size distribution
+		"Provides number of flows processed, batches processed, and batch size stats (in number of flows)",
+		operational.TypeSummary,
+		"stage",
+	)
+	batchSizeBytesSummary = operational.DefineMetric(
+		"ingest_batch_size_bytes",
+		"Ingested batch size distribution, in bytes",
+		operational.TypeSummary,
+		"stage",
+	)
+	errorsCounter = operational.DefineMetric(
+		"ingest_errors",
+		"Counter of errors during ingestion",
+		operational.TypeCounter,
+		"stage", "type", "code",
+	)
+)
+
 type metrics struct {
 	*operational.Metrics
 	stage          string
-	flowsProcessed prometheus.Counter
+	stageType      string
+	stageDuration  prometheus.Observer
+	latency        prometheus.Histogram
+	batchSize      prometheus.Summary
+	batchSizeBytes prometheus.Summary
+	errors         *prometheus.CounterVec
 }
 
-func newMetrics(opMetrics *operational.Metrics, stage string, inGaugeFunc func() int) *metrics {
+func newMetrics(opMetrics *operational.Metrics, stage, stageType string, inGaugeFunc func() int) *metrics {
 	opMetrics.CreateInQueueSizeGauge(stage, inGaugeFunc)
 	return &metrics{
 		Metrics:        opMetrics,
 		stage:          stage,
-		flowsProcessed: opMetrics.CreateFlowsProcessedCounter(stage),
+		stageType:      stageType,
+		latency:        opMetrics.NewHistogram(&latencyHistogram, []float64{.001, .01, .1, 1, 10, 100, 1000, 10000}, stage),
+		stageDuration:  opMetrics.GetOrCreateStageDurationHisto().WithLabelValues(stage),
+		batchSize:      opMetrics.NewSummary(&batchSizeSummary, stage),
+		batchSizeBytes: opMetrics.NewSummary(&batchSizeBytesSummary, stage),
+		errors:         opMetrics.NewCounterVec(&errorsCounter),
 	}
 }
 
 func (m *metrics) createOutQueueLen(out chan<- []config.GenericMap) {
 	m.CreateOutQueueSizeGauge(m.stage, func() int { return len(out) })
+}
+
+// Increment error counter
+// `code` should reflect any error code relative to this type. It can be a short string message,
+// but make sure to not include any dynamic value with high cardinality
+func (m *metrics) error(code string) {
+	m.errors.WithLabelValues(m.stage, m.stageType, code).Inc()
+}
+
+func (m *metrics) stageDurationTimer() *operational.Timer {
+	return operational.NewTimer(m.stageDuration)
 }
