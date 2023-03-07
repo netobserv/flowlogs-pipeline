fb37278dbb63915e3b117c139cf5bc09350d6695
Joel Takvorian <jtakvori@redhat.com>
2023-03-07 09:46:02 +0100

    Increasing test coverage
    - k8s enrichment
    - convert utils

--- pkg/pipeline/transform/kubernetes/kubernetes_test.go
+++ pkg/pipeline/transform/kubernetes/kubernetes_test.go
@@ -47,50 +47,161 @@ func (indexMock *IndexerMock) ByIndex(indexName, indexedValue string) ([]interfa
 	return args.Get(0).([]interface{}), args.Error(1)
 }
 
+func (indexMock *IndexerMock) GetByKey(key string) (interface{}, bool, error) {
+	args := indexMock.Called(key)
+	return args.Get(0), args.Bool(1), args.Error(2)
+}
+
 func (informerMock *InformerMock) GetIndexer() cache.Indexer {
 	args := informerMock.Called()
 	return args.Get(0).(cache.Indexer)
 }
 
-func TestGetInfoPods(t *testing.T) {
-	kubeData := KubeData{}
-	// pods informer
-	pidx := IndexerMock{}
-	pidx.On("ByIndex", IndexIP, "1.2.3.4").Return([]interface{}{&Info{
+func (m *IndexerMock) mockPod(ip, name, namespace, nodeIP string, owner *Owner) {
+	var ownerRef []metav1.OwnerReference
+	if owner != nil {
+		ownerRef = []metav1.OwnerReference{{
+			Kind: owner.Type,
+			Name: owner.Name,
+		}}
+	}
+	m.On("ByIndex", IndexIP, ip).Return([]interface{}{&Info{
 		Type: "Pod",
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      "podName",
-			Namespace: "podNamespace",
+			Name:            name,
+			Namespace:       namespace,
+			OwnerReferences: ownerRef,
 		},
-		HostIP: "10.0.0.1",
-		Owner:  Owner{Name: "podName", Type: "Pod"},
+		HostIP: nodeIP,
 	}}, nil)
-	pim := InformerMock{}
-	pim.On("GetIndexer").Return(&pidx)
-	// nodes informer
-	hidx := IndexerMock{}
-	hidx.On("ByIndex", IndexIP, "10.0.0.1").Return([]interface{}{&Info{
-		Type: "Node",
-		ObjectMeta: metav1.ObjectMeta{
-			Name: "nodeName",
-		},
-	}}, nil)
-	him := InformerMock{}
-	him.On("GetIndexer").Return(&hidx)
+}
 
-	kubeData.pods = &pim
-	kubeData.nodes = &him
+func (m *IndexerMock) mockNode(ip, name string) {
+	m.On("ByIndex", IndexIP, ip).Return([]interface{}{&Info{
+		Type:       "Node",
+		ObjectMeta: metav1.ObjectMeta{Name: name},
+	}}, nil)
+}
+
+func (m *IndexerMock) mockService(ip, name, namespace string) {
+	m.On("ByIndex", IndexIP, ip).Return([]interface{}{&Info{
+		Type:       "Service",
+		ObjectMeta: metav1.ObjectMeta{Name: name, Namespace: namespace},
+	}}, nil)
+}
+
+func (m *IndexerMock) mockReplicaSet(name, namespace string, owner Owner) {
+	m.On("GetByKey", namespace+"/"+name).Return(&metav1.ObjectMeta{
+		Name: name,
+		OwnerReferences: []metav1.OwnerReference{{
+			Kind: owner.Type,
+			Name: owner.Name,
+		}},
+	}, true, nil)
+}
+
+func (m *IndexerMock) fallbackNotFound() {
+	m.On("ByIndex", IndexIP, mock.Anything).Return([]interface{}{}, nil)
+}
+
+func setupMocks(kd *KubeData) (pods, nodes, svc, rs *IndexerMock) {
+	// pods informer
+	pods = &IndexerMock{}
+	pim := InformerMock{}
+	pim.On("GetIndexer").Return(pods)
+	kd.pods = &pim
+	// nodes informer
+	nodes = &IndexerMock{}
+	him := InformerMock{}
+	him.On("GetIndexer").Return(nodes)
+	kd.nodes = &him
+	// svc informer
+	svc = &IndexerMock{}
+	sim := InformerMock{}
+	sim.On("GetIndexer").Return(svc)
+	kd.services = &sim
+	// rs informer
+	rs = &IndexerMock{}
+	rim := InformerMock{}
+	rim.On("GetIndexer").Return(rs)
+	kd.replicaSets = &rim
+	return
+}
+
+func TestGetInfo(t *testing.T) {
+	kubeData := KubeData{}
+	pidx, hidx, sidx, ridx := setupMocks(&kubeData)
+	pidx.mockPod("1.2.3.4", "pod1", "podNamespace", "10.0.0.1", nil)
+	pidx.mockPod("1.2.3.5", "pod2", "podNamespace", "10.0.0.1", &Owner{Name: "rs1", Type: "ReplicaSet"})
+	pidx.fallbackNotFound()
+	ridx.mockReplicaSet("rs1", "podNamespace", Owner{Name: "dep1", Type: "Deployment"})
+	ridx.fallbackNotFound()
+	sidx.mockService("1.2.3.100", "svc1", "svcNamespace")
+	sidx.fallbackNotFound()
+	hidx.mockNode("10.0.0.1", "node1")
+	hidx.fallbackNotFound()
+
+	// Test get orphan pod
 	info, err := kubeData.GetInfo("1.2.3.4")
 	require.NoError(t, err)
 
 	require.Equal(t, *info, Info{
 		Type: "Pod",
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      "podName",
+			Name:      "pod1",
 			Namespace: "podNamespace",
 		},
-		HostName: "nodeName",
+		HostName: "node1",
 		HostIP:   "10.0.0.1",
-		Owner:    Owner{Name: "podName", Type: "Pod"},
+		Owner:    Owner{Name: "pod1", Type: "Pod"},
 	})
+
+	// Test get pod owned
+	info, err = kubeData.GetInfo("1.2.3.5")
+	require.NoError(t, err)
+
+	require.Equal(t, *info, Info{
+		Type: "Pod",
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "pod2",
+			Namespace: "podNamespace",
+			OwnerReferences: []metav1.OwnerReference{{
+				Kind: "ReplicaSet",
+				Name: "rs1",
+			}},
+		},
+		HostName: "node1",
+		HostIP:   "10.0.0.1",
+		Owner:    Owner{Name: "dep1", Type: "Deployment"},
+	})
+
+	// Test get node
+	info, err = kubeData.GetInfo("10.0.0.1")
+	require.NoError(t, err)
+
+	require.Equal(t, *info, Info{
+		Type: "Node",
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "node1",
+		},
+		Owner: Owner{Name: "node1", Type: "Node"},
+	})
+
+	// Test get service
+	info, err = kubeData.GetInfo("1.2.3.100")
+	require.NoError(t, err)
+
+	require.Equal(t, *info, Info{
+		Type: "Service",
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "svc1",
+			Namespace: "svcNamespace",
+		},
+		Owner: Owner{Name: "svc1", Type: "Service"},
+	})
+
+	// Test no match
+	info, err = kubeData.GetInfo("1.2.3.200")
+	require.NotNil(t, err)
+	require.Nil(t, info)
 }
---
+++ pkg/utils/convert_test.go
@@ -0,0 +1,115 @@
+package utils
+
+import (
+	"fmt"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestConvertToFloat64(t *testing.T) {
+	type tt struct {
+		input   interface{}
+		wantf64 float64
+		wantu32 uint32
+		wantu64 uint64
+		wanti64 int64
+		wanti   int
+	}
+	cases := []tt{{
+		input:   float64(1.1),
+		wantf64: 1.1,
+		wantu32: 1,
+		wantu64: 1,
+		wanti64: 1,
+		wanti:   1,
+	}, {
+		input:   float32(1.1),
+		wantf64: 1.1,
+		wantu32: 1,
+		wantu64: 1,
+		wanti64: 1,
+		wanti:   1,
+	}, {
+		input:   "1",
+		wantf64: 1,
+		wantu32: 1,
+		wantu64: 1,
+		wanti64: 1,
+		wanti:   1,
+	}, {
+		input:   int32(1),
+		wantf64: 1.0,
+		wantu32: 1,
+		wantu64: 1,
+		wanti64: 1,
+		wanti:   1,
+	}, {
+		input:   int64(1),
+		wantf64: 1.0,
+		wantu32: 1,
+		wantu64: 1,
+		wanti64: 1,
+		wanti:   1,
+	}, {
+		input:   int(1),
+		wantf64: 1.0,
+		wantu32: 1,
+		wantu64: 1,
+		wanti64: 1,
+		wanti:   1,
+	}, {
+		input:   uint32(1),
+		wantf64: 1.0,
+		wantu32: 1,
+		wantu64: 1,
+		wanti64: 1,
+		wanti:   1,
+	}, {
+		input:   uint64(1),
+		wantf64: 1.0,
+		wantu32: 1,
+		wantu64: 1,
+		wanti64: 1,
+		wanti:   1,
+	}, {
+		input:   uint(1),
+		wantf64: 1.0,
+		wantu32: 1,
+		wantu64: 1,
+		wanti64: 1,
+		wanti:   1,
+	}, {
+		input:   time.Duration(42),
+		wantf64: 42.0,
+		wantu32: 42,
+		wantu64: 42,
+		wanti64: 42,
+		wanti:   42,
+	}}
+	for _, tc := range cases {
+		tc := tc
+		t.Run(fmt.Sprintf("%t", tc.input), func(t *testing.T) {
+			f, err := ConvertToFloat64(tc.input)
+			assert.NoError(t, err)
+			assert.InDelta(t, tc.wantf64, f, 0.001)
+
+			u64, err := ConvertToUint64(tc.input)
+			assert.NoError(t, err)
+			assert.Equal(t, tc.wantu64, u64)
+
+			u32, err := ConvertToUint32(tc.input)
+			assert.NoError(t, err)
+			assert.Equal(t, tc.wantu32, u32)
+
+			i64, err := ConvertToInt64(tc.input)
+			assert.NoError(t, err)
+			assert.Equal(t, tc.wanti64, i64)
+
+			i, err := ConvertToInt(tc.input)
+			assert.NoError(t, err)
+			assert.Equal(t, tc.wanti, i)
+		})
+	}
+}
