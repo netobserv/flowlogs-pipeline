0269dcc8c4e2a9a555d3c9b12fb0c45d45f5e33e
Joel Takvorian <joel.takvorian@qaraywa.net>
2023-03-09 15:02:45 +0100

    Increasing test coverage (#401)
    * Increasing test coverage
    
    - k8s enrichment
    - convert utils
    
    * addressing feedback

--- pkg/pipeline/transform/kubernetes/kubernetes_test.go
+++ pkg/pipeline/transform/kubernetes/kubernetes_test.go
@@ -20,77 +20,84 @@ package kubernetes
 import (
 	"testing"
 
-	"github.com/stretchr/testify/mock"
 	"github.com/stretchr/testify/require"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-	"k8s.io/client-go/tools/cache"
 )
 
-type IndexerMock struct {
-	mock.Mock
-	cache.Indexer
-}
-
-type InformerMock struct {
-	mock.Mock
-	InformerInterface
-}
-
-type InformerInterface interface {
-	cache.SharedInformer
-	AddIndexers(indexers cache.Indexers) error
-	GetIndexer() cache.Indexer
-}
-
-func (indexMock *IndexerMock) ByIndex(indexName, indexedValue string) ([]interface{}, error) {
-	args := indexMock.Called(indexName, indexedValue)
-	return args.Get(0).([]interface{}), args.Error(1)
-}
-
-func (informerMock *InformerMock) GetIndexer() cache.Indexer {
-	args := informerMock.Called()
-	return args.Get(0).(cache.Indexer)
-}
-
-func TestGetInfoPods(t *testing.T) {
+func TestGetInfo(t *testing.T) {
 	kubeData := KubeData{}
-	// pods informer
-	pidx := IndexerMock{}
-	pidx.On("ByIndex", IndexIP, "1.2.3.4").Return([]interface{}{&Info{
-		Type: "Pod",
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      "podName",
-			Namespace: "podNamespace",
-		},
-		HostIP: "10.0.0.1",
-		Owner:  Owner{Name: "podName", Type: "Pod"},
-	}}, nil)
-	pim := InformerMock{}
-	pim.On("GetIndexer").Return(&pidx)
-	// nodes informer
-	hidx := IndexerMock{}
-	hidx.On("ByIndex", IndexIP, "10.0.0.1").Return([]interface{}{&Info{
-		Type: "Node",
-		ObjectMeta: metav1.ObjectMeta{
-			Name: "nodeName",
-		},
-	}}, nil)
-	him := InformerMock{}
-	him.On("GetIndexer").Return(&hidx)
+	pidx, hidx, sidx, ridx := SetupIndexerMocks(&kubeData)
+	pidx.MockPod("1.2.3.4", "pod1", "podNamespace", "10.0.0.1", nil)
+	pidx.MockPod("1.2.3.5", "pod2", "podNamespace", "10.0.0.1", &Owner{Name: "rs1", Type: "ReplicaSet"})
+	pidx.FallbackNotFound()
+	ridx.MockReplicaSet("rs1", "podNamespace", Owner{Name: "dep1", Type: "Deployment"})
+	ridx.FallbackNotFound()
+	sidx.MockService("1.2.3.100", "svc1", "svcNamespace")
+	sidx.FallbackNotFound()
+	hidx.MockNode("10.0.0.1", "node1")
+	hidx.FallbackNotFound()
 
-	kubeData.pods = &pim
-	kubeData.nodes = &him
+	// Test get orphan pod
 	info, err := kubeData.GetInfo("1.2.3.4")
 	require.NoError(t, err)
 
 	require.Equal(t, *info, Info{
 		Type: "Pod",
 		ObjectMeta: metav1.ObjectMeta{
-			Name:      "podName",
+			Name:      "pod1",
 			Namespace: "podNamespace",
 		},
-		HostName: "nodeName",
+		HostName: "node1",
 		HostIP:   "10.0.0.1",
-		Owner:    Owner{Name: "podName", Type: "Pod"},
+		Owner:    Owner{Name: "pod1", Type: "Pod"},
 	})
+
+	// Test get pod owned
+	info, err = kubeData.GetInfo("1.2.3.5")
+	require.NoError(t, err)
+
+	require.Equal(t, *info, Info{
+		Type: "Pod",
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "pod2",
+			Namespace: "podNamespace",
+			OwnerReferences: []metav1.OwnerReference{{
+				Kind: "ReplicaSet",
+				Name: "rs1",
+			}},
+		},
+		HostName: "node1",
+		HostIP:   "10.0.0.1",
+		Owner:    Owner{Name: "dep1", Type: "Deployment"},
+	})
+
+	// Test get node
+	info, err = kubeData.GetInfo("10.0.0.1")
+	require.NoError(t, err)
+
+	require.Equal(t, *info, Info{
+		Type: "Node",
+		ObjectMeta: metav1.ObjectMeta{
+			Name: "node1",
+		},
+		Owner: Owner{Name: "node1", Type: "Node"},
+	})
+
+	// Test get service
+	info, err = kubeData.GetInfo("1.2.3.100")
+	require.NoError(t, err)
+
+	require.Equal(t, *info, Info{
+		Type: "Service",
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      "svc1",
+			Namespace: "svcNamespace",
+		},
+		Owner: Owner{Name: "svc1", Type: "Service"},
+	})
+
+	// Test no match
+	info, err = kubeData.GetInfo("1.2.3.200")
+	require.NotNil(t, err)
+	require.Nil(t, info)
 }
