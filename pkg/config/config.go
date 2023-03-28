a31a0c0fbce9aa79de1588b4f2b1ef1dacb42749
KalmanMeth <meth@il.ibm.com>
2023-03-28 14:50:11 +0300

    Add ingest stage to provide synthetic workload for benchmarks (#395)
    * move GenerateConnectionFlowEntries from test to utils
    
    * added fields to GenerateConnectionEntries
    
    * added infrastructure for ingest synthetic
    
    * added ingest synthetic
    
    * added operational metric to count flow logs produced
    
    * added error message if metricsSettings missing
    
    * addressed reviewer comments
    
    * simplified ingest loop

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -90,6 +90,7 @@ type Ingest struct {
 	Collector *api.IngestCollector `yaml:"collector,omitempty" json:"collector,omitempty"`
 	Kafka     *api.IngestKafka     `yaml:"kafka,omitempty" json:"kafka,omitempty"`
 	GRPC      *api.IngestGRPCProto `yaml:"grpc,omitempty" json:"grpc,omitempty"`
+	Synthetic *api.IngestSynthetic `yaml:"synthetic,omitempty" json:"synthetic,omitempty"`
 }
 
 type File struct {
@@ -153,6 +154,8 @@ func ParseConfig(opts Options) (ConfigFileStruct, error) {
 			return out, err
 		}
 		logrus.Debugf("metrics settings = %v ", out.MetricsSettings)
+	} else {
+		logrus.Errorf("metrics settings missing")
 	}
 
 	return out, nil
