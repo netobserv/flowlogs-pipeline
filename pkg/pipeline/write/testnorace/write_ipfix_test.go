91917e3a15079c39ce037a6001ebf2c77a56b7f9
Brandon <bhale@users.noreply.github.com>
2024-02-26 10:18:16 -0500

    Add new field to ipfix exporter for flowRtt (#603)
    * add new field to ipfix exporter for flowRtt
    
    * go fmt
    
    * fix type on timeFlowRttNs field
    
    * rebase
    
    * rebase
    
    * add test case for timeflowrttns
    
    * fix type in ipfix unit test
    
    * increase number of returned fields in ipfix test
    
    * fix type in ipfix unit test
    
    * fix type in ipfix unit test
    
    * fix
    
    * fmt
    
    * year 2024
    
    ---------
    
    Co-authored-by: Brandon Hale <brandon.hale@ibm.com>

--- pkg/pipeline/write/testnorace/write_ipfix_test.go
+++ pkg/pipeline/write/testnorace/write_ipfix_test.go
@@ -119,7 +119,7 @@ func TestEnrichedIPFIXFlow(t *testing.T) {
 	assert.Equal(t, uint16(10), tplv4Msg.GetVersion())
 	templateSet := tplv4Msg.GetSet()
 	templateElements := templateSet.GetRecords()[0].GetOrderedElementList()
-	assert.Len(t, templateElements, 20)
+	assert.Len(t, templateElements, 21)
 	assert.Equal(t, uint32(0), templateElements[0].GetInfoElement().EnterpriseId)
 
 	// Check data
@@ -176,6 +176,8 @@ func matchElement(t *testing.T, element entities.InfoElementWithValue, flow conf
 		assert.Equal(t, flow["SrcK8S_HostName"], element.GetStringValue())
 	case "destinationNodeName":
 		assert.Equal(t, flow["DstK8S_HostName"], element.GetStringValue())
+	case "timeFlowRttNs":
+		assert.Equal(t, uint64(flow["TimeFlowRttNs"].(int64)), element.GetUnsigned64Value())
 	case "sourceMacAddress":
 	case "destinationMacAddress":
 		// Getting some discrepancies here, need to figure out why
