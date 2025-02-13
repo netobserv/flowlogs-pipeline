netobserv/flowlogs2metrics

Add ipsec support
Signed-off-by: Mohamed Mahmoud <mmahmoud@redhat.com>

vendor/github.com/netobserv/netobserv-ebpf-agent/pkg/ebpf/bpf_arm64_bpfel.go
@@ -22,7 +22,8 @@ type BpfAdditionalMetrics struct {
 	TranslatedFlow   BpfTranslatedFlowT
 	EthProtocol      uint16
 	NetworkEventsIdx uint8
-	_                [7]byte
+	FlowEncrypted    bool
+	_                [6]byte
 }
 
 type BpfDirectionT uint32
@@ -217,9 +218,10 @@ func LoadBpfObjects(obj interface{}, opts *ebpf.CollectionOptions) error {
 type BpfSpecs struct {
 	BpfProgramSpecs
 	BpfMapSpecs
+	BpfVariableSpecs
 }
 
-// BpfSpecs contains programs before they are loaded into the kernel.
+// BpfProgramSpecs contains programs before they are loaded into the kernel.
 //
 // It can be passed ebpf.CollectionSpec.Assign.
 type BpfProgramSpecs struct {
@@ -236,6 +238,10 @@ type BpfProgramSpecs struct {
 	TcxIngressFlowParse       *ebpf.ProgramSpec `ebpf:"tcx_ingress_flow_parse"`
 	TcxIngressPcaParse        *ebpf.ProgramSpec `ebpf:"tcx_ingress_pca_parse"`
 	TrackNatManipPkt          *ebpf.ProgramSpec `ebpf:"track_nat_manip_pkt"`
+	XfrmInputKprobe           *ebpf.ProgramSpec `ebpf:"xfrm_input_kprobe"`
+	XfrmInputKretprobe        *ebpf.ProgramSpec `ebpf:"xfrm_input_kretprobe"`
+	XfrmOutputKprobe          *ebpf.ProgramSpec `ebpf:"xfrm_output_kprobe"`
+	XfrmOutputKretprobe       *ebpf.ProgramSpec `ebpf:"xfrm_output_kretprobe"`
 }
 
 // BpfMapSpecs contains maps before they are loaded into the kernel.
@@ -250,6 +256,30 @@ type BpfMapSpecs struct {
 	GlobalCounters        *ebpf.MapSpec `ebpf:"global_counters"`
 	PacketRecord          *ebpf.MapSpec `ebpf:"packet_record"`
 	PeerFilterMap         *ebpf.MapSpec `ebpf:"peer_filter_map"`
+	SkBuffsEgressMap      *ebpf.MapSpec `ebpf:"sk_buffs_egress_map"`
+	SkBuffsIngressMap     *ebpf.MapSpec `ebpf:"sk_buffs_ingress_map"`
+}
+
+// BpfVariableSpecs contains global variables before they are loaded into the kernel.
+//
+// It can be passed ebpf.CollectionSpec.Assign.
+type BpfVariableSpecs struct {
+	DnsPort                        *ebpf.VariableSpec `ebpf:"dns_port"`
+	EnableDnsTracking              *ebpf.VariableSpec `ebpf:"enable_dns_tracking"`
+	EnableFlowsFiltering           *ebpf.VariableSpec `ebpf:"enable_flows_filtering"`
+	EnableIpsec                    *ebpf.VariableSpec `ebpf:"enable_ipsec"`
+	EnableNetworkEventsMonitoring  *ebpf.VariableSpec `ebpf:"enable_network_events_monitoring"`
+	EnablePca                      *ebpf.VariableSpec `ebpf:"enable_pca"`
+	EnablePktTranslationTracking   *ebpf.VariableSpec `ebpf:"enable_pkt_translation_tracking"`
+	EnableRtt                      *ebpf.VariableSpec `ebpf:"enable_rtt"`
+	FilterKey                      *ebpf.VariableSpec `ebpf:"filter_key"`
+	FilterValue                    *ebpf.VariableSpec `ebpf:"filter_value"`
+	HasFilterSampling              *ebpf.VariableSpec `ebpf:"has_filter_sampling"`
+	NetworkEventsMonitoringGroupid *ebpf.VariableSpec `ebpf:"network_events_monitoring_groupid"`
+	Sampling                       *ebpf.VariableSpec `ebpf:"sampling"`
+	TraceMessages                  *ebpf.VariableSpec `ebpf:"trace_messages"`
+	Unused8                        *ebpf.VariableSpec `ebpf:"unused8"`
+	Unused9                        *ebpf.VariableSpec `ebpf:"unused9"`
 }
 
 // BpfObjects contains all objects after they have been loaded into the kernel.
@@ -258,6 +288,7 @@ type BpfMapSpecs struct {
 type BpfObjects struct {
 	BpfPrograms
 	BpfMaps
+	BpfVariables
 }
 
 func (o *BpfObjects) Close() error {
@@ -279,6 +310,8 @@ type BpfMaps struct {
 	GlobalCounters        *ebpf.Map `ebpf:"global_counters"`
 	PacketRecord          *ebpf.Map `ebpf:"packet_record"`
 	PeerFilterMap         *ebpf.Map `ebpf:"peer_filter_map"`
+	SkBuffsEgressMap      *ebpf.Map `ebpf:"sk_buffs_egress_map"`
+	SkBuffsIngressMap     *ebpf.Map `ebpf:"sk_buffs_ingress_map"`
 }
 
 func (m *BpfMaps) Close() error {
@@ -291,7 +324,31 @@ func (m *BpfMaps) Close() error {
 		m.GlobalCounters,
 		m.PacketRecord,
 		m.PeerFilterMap,
+		m.SkBuffsEgressMap,
+		m.SkBuffsIngressMap,
 	)
+}
+
+// BpfVariables contains all global variables after they have been loaded into the kernel.
+//
+// It can be passed to LoadBpfObjects or ebpf.CollectionSpec.LoadAndAssign.
+type BpfVariables struct {
+	DnsPort                        *ebpf.Variable `ebpf:"dns_port"`
+	EnableDnsTracking              *ebpf.Variable `ebpf:"enable_dns_tracking"`
+	EnableFlowsFiltering           *ebpf.Variable `ebpf:"enable_flows_filtering"`
+	EnableIpsec                    *ebpf.Variable `ebpf:"enable_ipsec"`
+	EnableNetworkEventsMonitoring  *ebpf.Variable `ebpf:"enable_network_events_monitoring"`
+	EnablePca                      *ebpf.Variable `ebpf:"enable_pca"`
+	EnablePktTranslationTracking   *ebpf.Variable `ebpf:"enable_pkt_translation_tracking"`
+	EnableRtt                      *ebpf.Variable `ebpf:"enable_rtt"`
+	FilterKey                      *ebpf.Variable `ebpf:"filter_key"`
+	FilterValue                    *ebpf.Variable `ebpf:"filter_value"`
+	HasFilterSampling              *ebpf.Variable `ebpf:"has_filter_sampling"`
+	NetworkEventsMonitoringGroupid *ebpf.Variable `ebpf:"network_events_monitoring_groupid"`
+	Sampling                       *ebpf.Variable `ebpf:"sampling"`
+	TraceMessages                  *ebpf.Variable `ebpf:"trace_messages"`
+	Unused8                        *ebpf.Variable `ebpf:"unused8"`
+	Unused9                        *ebpf.Variable `ebpf:"unused9"`
 }
 
 // BpfPrograms contains all programs after they have been loaded into the kernel.
@@ -311,6 +368,10 @@ type BpfPrograms struct {
 	TcxIngressFlowParse       *ebpf.Program `ebpf:"tcx_ingress_flow_parse"`
 	TcxIngressPcaParse        *ebpf.Program `ebpf:"tcx_ingress_pca_parse"`
 	TrackNatManipPkt          *ebpf.Program `ebpf:"track_nat_manip_pkt"`
+	XfrmInputKprobe           *ebpf.Program `ebpf:"xfrm_input_kprobe"`
+	XfrmInputKretprobe        *ebpf.Program `ebpf:"xfrm_input_kretprobe"`
+	XfrmOutputKprobe          *ebpf.Program `ebpf:"xfrm_output_kprobe"`
+	XfrmOutputKretprobe       *ebpf.Program `ebpf:"xfrm_output_kretprobe"`
 }
 
 func (p *BpfPrograms) Close() error {
@@ -328,6 +389,10 @@ func (p *BpfPrograms) Close() error {
 		p.TcxIngressFlowParse,
 		p.TcxIngressPcaParse,
 		p.TrackNatManipPkt,
+		p.XfrmInputKprobe,
+		p.XfrmInputKretprobe,
+		p.XfrmOutputKprobe,
+		p.XfrmOutputKretprobe,
 	)
 }
 

netobserv/flowlogs-pipeline

vendor/github.com/netobserv/netobserv-ebpf-agent/pkg/ebpf/bpf_arm64_bpfel.go
@@ -22,7 +22,9 @@ type BpfAdditionalMetrics struct {
 	TranslatedFlow   BpfTranslatedFlowT
 	EthProtocol      uint16
 	NetworkEventsIdx uint8
-	_                [7]byte
+	FlowEncrypted    bool
+	FlowEncryptedRet uint8
+	_                [5]byte
 }
 
 type BpfDirectionT uint32
@@ -217,9 +219,10 @@ func LoadBpfObjects(obj interface{}, opts *ebpf.CollectionOptions) error {
 type BpfSpecs struct {
 	BpfProgramSpecs
 	BpfMapSpecs
+	BpfVariableSpecs
 }
 
-// BpfSpecs contains programs before they are loaded into the kernel.
+// BpfProgramSpecs contains programs before they are loaded into the kernel.
 //
 // It can be passed ebpf.CollectionSpec.Assign.
 type BpfProgramSpecs struct {
@@ -236,6 +239,10 @@ type BpfProgramSpecs struct {
 	TcxIngressFlowParse       *ebpf.ProgramSpec `ebpf:"tcx_ingress_flow_parse"`
 	TcxIngressPcaParse        *ebpf.ProgramSpec `ebpf:"tcx_ingress_pca_parse"`
 	TrackNatManipPkt          *ebpf.ProgramSpec `ebpf:"track_nat_manip_pkt"`
+	XfrmInputKprobe           *ebpf.ProgramSpec `ebpf:"xfrm_input_kprobe"`
+	XfrmInputKretprobe        *ebpf.ProgramSpec `ebpf:"xfrm_input_kretprobe"`
+	XfrmOutputKprobe          *ebpf.ProgramSpec `ebpf:"xfrm_output_kprobe"`
+	XfrmOutputKretprobe       *ebpf.ProgramSpec `ebpf:"xfrm_output_kretprobe"`
 }
 
 // BpfMapSpecs contains maps before they are loaded into the kernel.
@@ -248,8 +255,32 @@ type BpfMapSpecs struct {
 	DnsFlows              *ebpf.MapSpec `ebpf:"dns_flows"`
 	FilterMap             *ebpf.MapSpec `ebpf:"filter_map"`
 	GlobalCounters        *ebpf.MapSpec `ebpf:"global_counters"`
+	IpsecEgressMap        *ebpf.MapSpec `ebpf:"ipsec_egress_map"`
+	IpsecIngressMap       *ebpf.MapSpec `ebpf:"ipsec_ingress_map"`
 	PacketRecord          *ebpf.MapSpec `ebpf:"packet_record"`
 	PeerFilterMap         *ebpf.MapSpec `ebpf:"peer_filter_map"`
+}
+
+// BpfVariableSpecs contains global variables before they are loaded into the kernel.
+//
+// It can be passed ebpf.CollectionSpec.Assign.
+type BpfVariableSpecs struct {
+	DnsPort                        *ebpf.VariableSpec `ebpf:"dns_port"`
+	EnableDnsTracking              *ebpf.VariableSpec `ebpf:"enable_dns_tracking"`
+	EnableFlowsFiltering           *ebpf.VariableSpec `ebpf:"enable_flows_filtering"`
+	EnableIpsec                    *ebpf.VariableSpec `ebpf:"enable_ipsec"`
+	EnableNetworkEventsMonitoring  *ebpf.VariableSpec `ebpf:"enable_network_events_monitoring"`
+	EnablePca                      *ebpf.VariableSpec `ebpf:"enable_pca"`
+	EnablePktTranslationTracking   *ebpf.VariableSpec `ebpf:"enable_pkt_translation_tracking"`
+	EnableRtt                      *ebpf.VariableSpec `ebpf:"enable_rtt"`
+	FilterKey                      *ebpf.VariableSpec `ebpf:"filter_key"`
+	FilterValue                    *ebpf.VariableSpec `ebpf:"filter_value"`
+	HasFilterSampling              *ebpf.VariableSpec `ebpf:"has_filter_sampling"`
+	NetworkEventsMonitoringGroupid *ebpf.VariableSpec `ebpf:"network_events_monitoring_groupid"`
+	Sampling                       *ebpf.VariableSpec `ebpf:"sampling"`
+	TraceMessages                  *ebpf.VariableSpec `ebpf:"trace_messages"`
+	Unused8                        *ebpf.VariableSpec `ebpf:"unused8"`
+	Unused9                        *ebpf.VariableSpec `ebpf:"unused9"`
 }
 
 // BpfObjects contains all objects after they have been loaded into the kernel.
@@ -258,6 +289,7 @@ type BpfMapSpecs struct {
 type BpfObjects struct {
 	BpfPrograms
 	BpfMaps
+	BpfVariables
 }
 
 func (o *BpfObjects) Close() error {
@@ -277,6 +309,8 @@ type BpfMaps struct {
 	DnsFlows              *ebpf.Map `ebpf:"dns_flows"`
 	FilterMap             *ebpf.Map `ebpf:"filter_map"`
 	GlobalCounters        *ebpf.Map `ebpf:"global_counters"`
+	IpsecEgressMap        *ebpf.Map `ebpf:"ipsec_egress_map"`
+	IpsecIngressMap       *ebpf.Map `ebpf:"ipsec_ingress_map"`
 	PacketRecord          *ebpf.Map `ebpf:"packet_record"`
 	PeerFilterMap         *ebpf.Map `ebpf:"peer_filter_map"`
 }
@@ -289,9 +323,33 @@ func (m *BpfMaps) Close() error {
 		m.DnsFlows,
 		m.FilterMap,
 		m.GlobalCounters,
+		m.IpsecEgressMap,
+		m.IpsecIngressMap,
 		m.PacketRecord,
 		m.PeerFilterMap,
 	)
+}
+
+// BpfVariables contains all global variables after they have been loaded into the kernel.
+//
+// It can be passed to LoadBpfObjects or ebpf.CollectionSpec.LoadAndAssign.
+type BpfVariables struct {
+	DnsPort                        *ebpf.Variable `ebpf:"dns_port"`
+	EnableDnsTracking              *ebpf.Variable `ebpf:"enable_dns_tracking"`
+	EnableFlowsFiltering           *ebpf.Variable `ebpf:"enable_flows_filtering"`
+	EnableIpsec                    *ebpf.Variable `ebpf:"enable_ipsec"`
+	EnableNetworkEventsMonitoring  *ebpf.Variable `ebpf:"enable_network_events_monitoring"`
+	EnablePca                      *ebpf.Variable `ebpf:"enable_pca"`
+	EnablePktTranslationTracking   *ebpf.Variable `ebpf:"enable_pkt_translation_tracking"`
+	EnableRtt                      *ebpf.Variable `ebpf:"enable_rtt"`
+	FilterKey                      *ebpf.Variable `ebpf:"filter_key"`
+	FilterValue                    *ebpf.Variable `ebpf:"filter_value"`
+	HasFilterSampling              *ebpf.Variable `ebpf:"has_filter_sampling"`
+	NetworkEventsMonitoringGroupid *ebpf.Variable `ebpf:"network_events_monitoring_groupid"`
+	Sampling                       *ebpf.Variable `ebpf:"sampling"`
+	TraceMessages                  *ebpf.Variable `ebpf:"trace_messages"`
+	Unused8                        *ebpf.Variable `ebpf:"unused8"`
+	Unused9                        *ebpf.Variable `ebpf:"unused9"`
 }
 
 // BpfPrograms contains all programs after they have been loaded into the kernel.
@@ -311,6 +369,10 @@ type BpfPrograms struct {
 	TcxIngressFlowParse       *ebpf.Program `ebpf:"tcx_ingress_flow_parse"`
 	TcxIngressPcaParse        *ebpf.Program `ebpf:"tcx_ingress_pca_parse"`
 	TrackNatManipPkt          *ebpf.Program `ebpf:"track_nat_manip_pkt"`
+	XfrmInputKprobe           *ebpf.Program `ebpf:"xfrm_input_kprobe"`
+	XfrmInputKretprobe        *ebpf.Program `ebpf:"xfrm_input_kretprobe"`
+	XfrmOutputKprobe          *ebpf.Program `ebpf:"xfrm_output_kprobe"`
+	XfrmOutputKretprobe       *ebpf.Program `ebpf:"xfrm_output_kretprobe"`
 }
 
 func (p *BpfPrograms) Close() error {
@@ -328,6 +390,10 @@ func (p *BpfPrograms) Close() error {
 		p.TcxIngressFlowParse,
 		p.TcxIngressPcaParse,
 		p.TrackNatManipPkt,
+		p.XfrmInputKprobe,
+		p.XfrmInputKretprobe,
+		p.XfrmOutputKprobe,
+		p.XfrmOutputKretprobe,
 	)
 }
 