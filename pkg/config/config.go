9b88712b2e7456511261fd8740906ba5b7712b97
Joel Takvorian <jtakvori@redhat.com>
2025-05-22 17:48:58 +0200

    Fix suppressGoMetrics, rename it to suppressDefaultMetrics
    Fixes #944

--- pkg/config/config.go
+++ pkg/config/config.go
@@ -75,7 +75,7 @@ type MetricsSettings struct {
 	DisableGlobalServer    bool   `yaml:"disableGlobalServer,omitempty" json:"disableGlobalServer,omitempty" doc:"disabling the global metrics server makes operational metrics unavailable. If prometheus-encoding stages are defined, they need to contain their own metrics server parameters."`
 	Prefix                 string `yaml:"prefix,omitempty" json:"prefix,omitempty" doc:"prefix for names of the operational metrics"`
 	NoPanic                bool   `yaml:"noPanic,omitempty" json:"noPanic,omitempty"`
-	SuppressGoMetrics      bool   `yaml:"suppressGoMetrics,omitempty" json:"suppressGoMetrics,omitempty" doc:"filter out Go and process metrics"`
+	SuppressDefaultMetrics bool   `yaml:"suppressDefaultMetrics,omitempty" json:"suppressDefaultMetrics,omitempty" doc:"filter out default Prometheus metrics such as Go and process metrics"`
 }
 
 // PerfSettings allows setting some internal configuration parameters
