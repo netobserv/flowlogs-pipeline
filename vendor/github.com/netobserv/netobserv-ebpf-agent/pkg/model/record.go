bff214fed5a0f9e445d23c397bda2b82483409bb
Joel Takvorian <jtakvori@redhat.com>
2025-10-31 10:01:43 +0100

    NETOBSERV-2471: TLS usage tracking (bump agent)

--- vendor/github.com/netobserv/netobserv-ebpf-agent/pkg/model/record.go
+++ vendor/github.com/netobserv/netobserv-ebpf-agent/pkg/model/record.go
@@ -1,6 +1,7 @@
 package model
 
 import (
+	"crypto/tls"
 	"encoding/binary"
 	"fmt"
 	"io"
@@ -25,6 +26,10 @@ const (
 	NetworkEventsMaxEventsMD = 8
 	MaxNetworkEvents         = 4
 	MaxObservedInterfaces    = 6
+
+	MiscFlagsSSLMismatch  = 0x01
+	TLSTrackerClientHello = 0x01
+	TLSTrackerServerHello = 0x02
 )
 
 var recordLog = logrus.WithField("component", "model")
@@ -223,4 +228,23 @@ func AllZeroIP(ip net.IP) bool {
 		return true
 	}
 	return false
+}
+
+func (r *BpfFlowContent) SSLVersionToString() string {
+	if r.SslVersion == 0 {
+		return ""
+	}
+	v := tls.VersionName(r.SslVersion)
+	if r.HasSSLMismatch() {
+		return "~ " + v
+	}
+	return v
+}
+
+func (r *BpfFlowContent) TLSTypesToStrings() []string {
+	return tlsTypesToStrings(r.TlsTypes)
+}
+
+func (r *BpfFlowContent) HasSSLMismatch() bool {
+	return r.MiscFlags&MiscFlagsSSLMismatch > 0
 }
